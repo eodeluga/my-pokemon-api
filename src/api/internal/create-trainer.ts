c19d68933feafe5716b5fa21eeea9591f87c8556
Eugene Odeluga <eugene.odeluga@agent3.com>
2023-04-05 11:48:15 +0100

    Added create-trainer trainer object validation

--- src/api/internal/create-trainer.ts
+++ src/api/internal/create-trainer.ts
@@ -1,4 +1,4 @@
-import { object, number } from "yup";
+import { object, string, ValidationError } from "yup";
 
 import { Response, Request } from 'express'
 
@@ -6,19 +6,47 @@ import service, { Trainer } from "../../services/TrainerService";
 import { prismaClient } from "../../services/db";
 
 export default async function createTrainerHandler(req: Request, res: Response) {
+  // Describe search schema
+  const trainerSchema = object().shape({
+    username: string()
+      .required('Username is required')
+      .min(4, 'Username must be at least 4 characters')
+      .max(20, 'Username must be less than 20 characters'),
+    password: string()
+      .required('Password is required')
+      .min(8, 'Password must be at least 8 characters')
+      .matches(
+        /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]+$/,
+        'Password must include at least one number and one special character'
+      ),
+    email_address: string()
+      .email('Invalid email address')
+      .required('Email is required'),
+    });
+  
   try {
     const trainerService = service(prismaClient);
-    const { data } = req.body;
-    const trainer: Trainer = await trainerService.create(data);
+    const { username, password, email_address } = await trainerSchema.validate(req.body.data);
+    const trainer: Trainer = await trainerService.create({
+        username,
+        password,
+        email_address
+    });
     res.status(200).send({
         id: trainer.id,
         username: trainer.username,
     })
-  } catch {
-    // Failure
+  } catch(err) {
+    
+    // Format the failure message before sending
+    const errObj = err as ValidationError;
+    String(errObj?.name).startsWith("ValidationError") 
+      ? err = `${errObj.name}: ${errObj.message}`
+      : err = "Invalid request";
+    
     res.status(400).send({
       status: 400,
-      msg: 'Invalid request',
+      msg: err,
     });
   }
 }
